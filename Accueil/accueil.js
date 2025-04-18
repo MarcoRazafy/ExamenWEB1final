9c5aa93e1b54c3133359afa127a9949788b97fa4
Sitraka <sitrakaandriamisaina96@gmail.com>
2025-04-18 22:34:59 +0300

    Finalisation des options

--- Accueil/accueil.js
+++ Accueil/accueil.js
@@ -1,23 +1,23 @@
-// const setting_button = document.getElementById('setting_button');
-// const settings_list= document.getElementById('settings_list');
+function ouvrir_option() {
+  document.getElementById("settings_list").style.display = "flex";
+}
 
-// setting_button.addEventListener('click', () => {
-//     if (settings_list.classList.contains('hidden')) {
-//     settings_list.classList.remove('hidden');
-//     // Force reflow to restart animation
-//     void settings_list.offsetWidth;
-//     settings_list.classList.add('show');
-// } else {
-//     settings_list.classList.add('hidden');
-//     settings_list.classList.remove('show');
-//   }
-// });
+function fermer_option() {
+  document.getElementById("settings_list").style.display = "none";
+}
 
+function applique_option() {
+  document.getElementById("settings_list").style.display = "none";
+}
 
-  function ouvrir_option() {
-    document.getElementById("settings_list").style.display = "flex";
-  }
+function defaut_option() {
+  document.getElementById("settings_list").style.display = "none";
+}
 
-  function fermer_option() {
-    document.getElementById("settings_list").style.display = "none";
-  }
+function ouvrir_propos() {
+  document.getElementById("about_content").style.display = "flex";
+}
+
+function fermer_propos() {
+  document.getElementById("about_content").style.display = "none";
+}

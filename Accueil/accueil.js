efbd08b6111f23862cc9e283cd4a16d488360b1d
Sitraka <sitrakaandriamisaina96@gmail.com>
2025-04-19 00:04:01 +0300

    Rectification des paramètres

--- Accueil/accueil.js
+++ Accueil/accueil.js
@@ -21,3 +21,47 @@ function ouvrir_propos() {
 function fermer_propos() {
   document.getElementById("about_content").style.display = "none";
 }
+
+function ouvrir_propos() {
+  const about = document.getElementById("about_content");
+  about.classList.remove("out");
+  about.classList.add("active");
+  about.style.display = "flex"; // pour s'assurer qu’il s’affiche
+}
+
+function fermer_propos() {
+  const about = document.getElementById("about_content");
+  about.classList.remove("active");
+  about.classList.add("out");
+
+  // attendre que l'animation de sortie finisse avant de cacher
+  setTimeout(() => {
+    about.style.display = "none";
+    about.classList.remove("out");
+  }, 400); // doit correspondre à la durée de fadeOut
+}
+
+
+
+
+
+
+
+
+
+
+function level_btn() {
+  document.getElementById("level_lists").style.display = "flex";
+}
+
+function level_retour1() {
+  document.getElementById("level_lists").style.display = "none";
+}
+
+function level_next() {
+  document.getElementById("next_level_lists").style.display = "flex";
+}
+
+function level_retour2() {
+  document.getElementById("next_level_lists").style.display = "none";
+}
